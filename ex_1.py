eb42c1f00f94a5d9101ebe22782a9cafbc8b7fd0
Koniokrad <osowski.grzegorz@gmail.com>
2018-03-30 09:57:54 +0200

    exercise 1 added datetime object

--- ex_1.py
+++ ex_1.py
@@ -1,7 +1,10 @@
+import datetime
+
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 number = int(input("Enter random number: "))
-year = 100 - age + 2018
+date = datetime.datetime.now().strftime("%Y")
+year = 100 - age + int(date)
 
 for x in range(0, number):
     print("{} in {} you will turn 100 years old".format(name, year))
