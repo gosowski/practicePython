ca6686bc07a6820abbe7683df09776289d69946c
Koniokrad <osowski.grzegorz@gmail.com>
2018-03-30 12:23:26 +0200

    exercise 6 improved

--- ex_6.py
+++ ex_6.py
@@ -1,6 +1,6 @@
 word = input("Enter word to check: ")
 
 if str(word) == str(word)[::-1]:
-    print("{} it is a palindrome".format(word))
+    print("{} is a palindrome".format(word))
 else:
-    print("{} it is not a palindrome".format(word))
+    print("{} is not a palindrome".format(word))
